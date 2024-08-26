96b2823660a1605b5eecdf4ca7955783f2b54591
janmatula <jan@alutam.com>
2024-08-26 13:40:45 +0200

    Removed inject into `onEntityCollision`. It was not necessary for desired detector rail behaviour and prevented detector rails from deactivating.

--- server/src/main/java/me/kimovoid/betacmp/mixin/rails/DetectorRailBlockMixin.java
+++ server/src/main/java/me/kimovoid/betacmp/mixin/rails/DetectorRailBlockMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.block.DetectorRailBlock;
 import net.minecraft.block.RailBlock;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
